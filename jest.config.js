701b9b6327c8959f67fbe723830efbc2edf89b9e
ChenNing02 <2633565885@qq.com>
2021-09-24 23:39:51 +0800

    安装 Jest 支持 JSX 语法相关的依赖，并且让 Jest 支持引入 stylus 等样式文件测试

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,20 @@
 module.exports = {
+  moduleDirectories: ['node_modules'],
+  testMatch: [
+    '<rootDir>/test/**/*.(spec|test).(ts|tsx)'
+  ],
   moduleFileExtensions: [
     'js',
-    'ts',
     'json',
+    'ts',
+    'tsx',
     'vue'
   ],
   transform: {
     '^.+\\.ts$': 'ts-jest',
-    '^.+\\.vue$': 'vue-jest'
+    '^.+\\.vue$': 'vue-jest',
+    // ".+\\.(css|styl|less|sass|scss)$": "jest-css-modules-transform",
+    ".+\\.(css|styl)$": "jest-css-modules-transform",
+    '^.+\\.(j|t)sx?$': 'babel-jest'
   },
 }
