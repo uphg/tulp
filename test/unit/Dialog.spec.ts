325182f744e4c6e1f55954cc2fd874b1bf9380dc
ChenNing02 <2633565885@qq.com>
2021-09-28 23:17:13 +0800

    给 Dialog 组件添加快捷调用的单元测试

--- test/unit/Dialog.spec.ts
+++ test/unit/Dialog.spec.ts
@@ -1,9 +1,26 @@
 import { mount } from '@vue/test-utils'
-import { Dialog } from '../../src/lib/index'
+import { defineComponent } from 'vue'
+import { Dialog, useDialog } from '../../src/lib/index'
 
 describe('Dialog', () => {
   it('create', () => {
     const wrapper = mount(Dialog)
     expect(wrapper.exists()).toBe(true)
+  })
+
+  it('can be quickly called by type', () => {
+    const wrapper = mount(defineComponent({
+      setup() {
+        const dialog = useDialog()
+        dialog.success({
+          title: 'Call success',
+          content: 'Content'
+        })
+      },
+      render() {
+        return null
+      }
+    }))
+    wrapper.unmount()
   })
 })
