fe18810fa7a19e5739b62a5a6cbd6d4563dc5642
chen <1834805770@qq.com>
2021-10-01 09:46:39 +0800

    将 lib 的 vite 配置同步至 docs

--- vite.config.docs.ts
+++ vite.config.docs.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base: '/tulip/',
+  plugins: [vue(), vueJsx()],
+  base: '/tulp/',
+  resolve: {
+    alias: [
+      {
+        find: 'src',
+        replacement: path.resolve(__dirname, './src')
+      }
+    ]
+  },
   build: {
     outDir: 'dist-docs'
   }
