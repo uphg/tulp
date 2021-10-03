f156ccf53d96ff71248e46efc673dd902b6da566
chen <1834805770@qq.com>
2021-10-03 20:46:20 +0800

    修改部署路径

--- deploy.sh
+++ deploy.sh
@@ -12,8 +12,8 @@ git add -A
 echo -e "\n# commit build"
 git commit -m 'deploy'
 echo -e "\n# push Github"
-git push -f git@github.com:uphg/tulip.git master:gh-pages
+git push -f git@github.com:uphg/tulp.git master:gh-pages
 echo -e "\n# push Gitee"
-git push -f git@gitee.com:uphg/tulip.git master:gh-pages
+git push -f git@gitee.com:uphg/tulp.git master:gh-pages
 echo -e "\n# Exit the build folder"
 cd -
