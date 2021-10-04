96ae2c7bb145beaf39f926185e1488974fcb6d30
chen <1834805770@qq.com>
2021-10-04 16:18:49 +0800

    修改 404 页面

--- src/Layout/404.vue
+++ src/Layout/404.vue
@@ -1,28 +1,23 @@
+<script>
+const message: string[] = [
+  `这里什么都没有。`,
+  `你是怎么来到这里的？`,
+  `这是 404。`,
+  `你可能已经断开连接了。`
+]
+</script>
+
+<script setup lang="ts">
+const getMsg = (): string => {
+  return message[Math.floor(Math.random() * message.length)]
+}
+</script>
+
 <template>
   <div class="vue-docs-container">
     <h1>404</h1>
-
     <blockquote>{{ getMsg() }}</blockquote>
-
     <router-link to="/">返回首页</router-link>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-
-const message: string[] = [
-  `这里什么都没有。`,
-  `你是怎么来到这里的？`,
-  `这是 404 哦。`,
-  `你可能已经断开连接。`
-]
-
-export default defineComponent({
-  methods: {
-    getMsg (): string {
-      return message[Math.floor(Math.random() * message.length)]
-    }
-  }
-})
-</script>
