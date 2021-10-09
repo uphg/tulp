6765847634fc8be3121692901f4fffc2674bb705
ChenNing02 <2633565885@qq.com>
2021-10-09 14:54:53 +0800

    调整首页布局样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,8 @@
-<script lang="ts" setup>
-import { Button } from '~src/lib/index';
-</script>
-
 <template>
   <div class="home">
     <div class="home-content">
       <h1 class="home-title">
-        Tulp
+        <span>Tulp</span>
       </h1>
       <div class="description">
         <p>一个 Vue 3 组件库</p>
@@ -23,30 +19,24 @@ import { Button } from '~src/lib/index';
 .home
   height 100%
 
-.home .page-wrapper
+.home-content
   height 100%
   display flex
   flex-direction column
   justify-content center
   align-items center
-
-.home-content {
-  text-align: center;
-  margin-top: -16vh;
-  .home-title{
-    font-size: 7em;
-    font-weight: bold;
-    // font-family: https://fonts.google.com/specimen/Bangers#standard-styles
-    font-family: Bangers, sans-serif;
-    text-align: center;
-    text-shadow: 6px 6px 0 #ffc947;
-    margin: 0 0 20px;
-    transition: font-size 0.25s;
-  }
-  .description {
-    color: #476582;
-    margin-bottom: 20px;
-  }
-}
+  text-align center
+  .home-title
+    font-size 7em
+    font-weight bold
+    // font-family https://fonts.google.com/specimen/Bangers#standard-styles
+    font-family Bangers, sans-serif
+    text-align center
+    text-shadow 6px 6px 0 #ffc947
+    margin -16vh 0 20px
+    transition font-size 0.25s
+  .description 
+    color #476582
+    margin-bottom 20px
 
 </style>
