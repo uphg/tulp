0963d354bb086f088c6f5373fd9396d5075d93de
ChenNing02 <2633565885@qq.com>
2021-10-11 12:29:00 +0800

    去掉 @vueuse/head 依赖

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,12 @@
 import { createApp } from 'vue'
 import { App } from './app/index'
 import { router } from './router/index'
-import { createHead } from '@vueuse/head' // <--
 import './styles/indes.styl'
 import Example from '~src/components/Example.vue'
 import { TButton, TInput, TCollapseTransition, TIcon } from '~src/lib/index'
 
 const app = createApp(App)
-const head = createHead() // <--
 
-app.use(head) // <--
 app.use(router)
 app.mount('#app')
 
