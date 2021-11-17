59e01410a01e5197dcbd2c395306c94e89ef7147
uphg <2633565885@qq.com>
2021-11-17 10:19:48 +0800

    build: 修改 path 引入方式，改为 import 引入

--- vite.config.common.ts
+++ vite.config.common.ts
@@ -3,13 +3,13 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import Markdown from 'vite-plugin-md'
 import { vueExampleCode } from './plugins/vue-example-code'
 
-const path = require('path')
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default {
   plugins: [
     vue({
-      include: [/\.vue$/, /\.md$/], // 添加 markdown 文件支持
+      include: [/\.vue$/, /\.md$/] // 添加 markdown 文件支持
     }),
     Markdown({
       // Class names for the wrapper div
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import commonOptions from './vite.config.common'
-
-const path = require('path')
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
