be2ab68537fc5061ab5b466de26651ff5717285c
uphg <2633565885@qq.com>
2021-11-19 16:42:15 +0800

    feat: 让 Switch 组件支持添加内容

--- src/lib/switch/src/Switch.vue
+++ src/lib/switch/src/Switch.vue
@@ -4,7 +4,25 @@
     :class="{ 'tu-switch--checked': value }"
     @click="toggle"
   >
-    <span v-if="isWave" class="tu-switch__wave" :class="{ active: isWave }" />
+    <span
+      v-if="isWave"
+      :class="[
+        'tu-switch__wave',
+        { active: isWave }
+      ]"
+    />
+    <span
+      v-if="$slots.checked"
+      class="tu-switch__checked"
+    >
+      <slot name="checked" />
+    </span>
+    <span
+      v-if="$slots.unchecked"
+      class="tu-switch__unchecked"
+    >
+      <slot name="unchecked" />
+    </span>
     <span class="tu-switch__core" />
   </button>
 </template>
@@ -15,7 +33,13 @@ import { useTriggerWave } from '../../_mixins/use-trigger-wave'
 
 export default defineComponent({
   props: {
-    value: Boolean
+    value: Boolean,
+    checkedText: {
+      type: [String, Number]
+    },
+    uncheckedText: {
+      type: [String, Number]
+    }
   },
   emits: ['update:value'],
   setup(props, context) {
--- src/views/components/switch.md
+++ src/views/components/switch.md
@@ -2,12 +2,21 @@
 title: Switch 组件
 ---
 
-switch 开关。
+用于切换开关状态的组件。
 
 ## 基础用法
 
+绑定 value 来切换 Switch 的状态。
+
 <Example class="switch-demo" :code="SwitchBase" />
+
+## 添加内容
+
+可以使用命名插槽添加内容描述。
+
+<Example class="switch-demo" :code="SwitchText" />
 
 <script setup lang="ts">
 import * as SwitchBase from '~src/example/switch/base.vue'
+import * as SwitchText from '~src/example/switch/text.vue'
 </script>
