297133b987b8c64f0fa6450e959f888c31bd16f3
uphg <2633565885@qq.com>
2022-03-11 10:41:26 +0800

    refactor: 调整 dialog 弹框隐藏 body 滚动条类名，优化隐藏滚动条相关方法

--- src/lib/_utils/body.ts
+++ src/lib/_utils/body.ts
@@ -9,14 +9,16 @@ const hasScrollBar = (): boolean => {
 }
 
 export const offBodyScroll = () => {
-  if (!hasScrollBar()) return
-  const scrollBarWidth = getScrollBarWidth()
-  body.style.setProperty('--tu-scrollbar-width', `${scrollBarWidth}px`)
-  body.classList.add('is-tu-components')
+  if (hasScrollBar()) {
+    const scrollBarWidth = getScrollBarWidth()
+    body.style.setProperty('--tu-scrollbar-width', `${scrollBarWidth}px`)
+    body.classList.add('tu-scrollbar-width')
+  }
 }
 
 export const onBodyScroll = () => {
-  if (!hasScrollBar()) return
-  body.style.removeProperty('--tu-scrollbar-width')
-  body.classList.remove('is-tu-components')
+  if (hasScrollBar()) {
+    body.style.removeProperty('--tu-scrollbar-width')
+    body.classList.remove('tu-scrollbar-width')
+  }
 }
